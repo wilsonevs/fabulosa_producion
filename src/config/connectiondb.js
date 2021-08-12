280504603bcf1fc959449fd3dd2364acb8aee2fb
wilsonevs <wilsonevs@gmail.com>
2021-08-11 23:31:03 -0500

    18

--- src/config/connectiondb.js
+++ src/config/connectiondb.js
@@ -10,29 +10,7 @@ const connectionDb = mysql.createConnection({
     pass: process.env.DB_PASSWORD,
 });
 
-//probar el modulo:
-
-function handleDisconnect(connectionDb) {
-
-    connection= mysql.createPool(connectionDb);
-
-    connection.getConnection(function (err) {
-        if(err){
-            console.log("error heroku to bd: ", err);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-
-    connection.on('Error', function (err) {
-        console.log('db error heroku: ', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();            
-        }else{
-            throw err;
-        }
-    });
-    console.log("WILSON ERROR"+connectionDb);
-}
 
 
-handleDisconnect(connectionDb);
+
+module.exports = connectionDb;
