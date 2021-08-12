66ac85b0e9d67ad5697ce781815dfcd506fb095a
wilsonevs <wilsonevs@gmail.com>
2021-08-12 15:10:48 -0500

    17

--- src/config/connectiondb.js
+++ src/config/connectiondb.js
@@ -3,21 +3,40 @@ const mysql = require('mysql');
 //funciona porque el servidor esta conectado con anterioridad a dotenv que esta configurado
 //y ligado a la carpeta ./env/.env
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    pass: process.env.DB_PASSWORD,
-});
+const dataConnection = {
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b7ba6f0ae3efd4',
+    database: 'heroku_b728dea260d0291',
+    password: 'd2564c03',
+};
 
-connection.connect((err) => {
-    if(err){
-        console.log("El error de conexion a BD es: " + err)
-        // return res.redirect('./500.ejs');
-    }
-    console.log("Conectado exitosamente a BD");
-})
+function handleDisconnect(dataConnection) {
+	const connection = mysql.createPool(dataConnection);
 
+	connection.getConnection((err) => {
+		if (err) {
+			console.log('Error de conexion con db:', err);
+			setTimeout(handleDisconnect, 2000);
+		} else {
+			console.log('Conectado exitosamente con la DB');
+		}
+	});
 
+	connection.on('error', (err) => {
+		console.log('Db error', err);
+		if (err.code === 'PROTOCOL_DataConnection_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
 
+	return connection;
+}
+
+const connection = handleDisconnect(dataConnection);
+
+// Exportacion de la conexion
 module.exports = connection;
+
+
